106738c0f46041c7eea28567c21218c493bcc5c4
Zach Banducci <zbducci@gmail.com>
2019-10-23 17:04:39 -0500

    Added jsdocs to functions

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+/**
+ * @typedef {Object} Gnome
+ */
+
 const gnome = (listeners = []) => {
+  /**
+   * @param event event name to be listened to
+   * @param handler callback function to be executed when the passed event is emitted
+   * @return {Gnome} gnome event emitter with event listener
+   */
   const on = (event, handler) => gnome([...listeners, {event, handler}])
   
+  /**
+   * @param event event name to be listened to
+   * @param data callback function to be executed when the passed event is emitted
+   * @return {Gnome} gnome event emitter with same listeners
+   */
   const emit = (event, data) => {
     listeners
       .filter(el => el.event === event)
@@ -9,8 +23,17 @@ const gnome = (listeners = []) => {
     return gnome(listeners)
   }
   
+  /**
+   * Returns an event emitter without any 
+   * @param event event name to be listened to
+   * @param data callback function to be executed when the passed event is emitted
+   * @return {Gnome} gnome event emitter without event
+   */
   const off = (event) => gnome(listeners.filter(el => el.event !== event))
 
+  /**
+   * @return {Gnome} gnome event emitter without listeners
+   */
   const close = () => gnome()
   
   return {
